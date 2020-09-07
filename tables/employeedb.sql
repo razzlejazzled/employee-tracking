2b8b8fbdf0c6b2c6e2e0a47be9e9c997a1296d40
RazzleJazzled <robertjgassner@gmail.com>
2020-09-07 13:31:51 -0700

    I think the database is complete

--- tables/employeedb.sql
+++ tables/employeedb.sql
@@ -13,18 +13,56 @@ CREATE TABLE roles(
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR (30) NOT NULL,
     salary DECIMAL,
+    departmentID INT,
     FOREIGN KEY (departmentID) REFERENCES department(id),
-    PRIMARY KEY (id),
+    PRIMARY KEY (id)
 );
 CREATE TABLE people(
     id INTEGER NOT NULL AUTO_INCREMENT,
     firstName VARCHAR (30) NOT NULL,
     lastName VARCHAR (30) NOT NULL,
+    roleID INT,
+    managerID INT,
     FOREIGN KEY (roleID) REFERENCES roles(id), 
     FOREIGN KEY (managerID) REFERENCES people(id),
-    PRIMARY KEY (id),
+    PRIMARY KEY (id)
 
 );
 
+INSERT INTO department (departmentName)
+VALUES ("Legal");
 
+INSERT INTO department (departmentName)
+VALUES ("Sales");
 
+INSERT INTO department (departmentName)
+VALUES ("Accounting");
+
+INSERT INTO department (departmentName)
+VALUES ("Software Development");
+
+INSERT INTO roles (title, salary, departmentID)
+VALUES ("Sales Lead", 65.000, 2);
+INSERT INTO roles (title, salary, departmentID)
+VALUES ("Sales Person", 45.000, 2);
+INSERT INTO roles (title, salary, departmentID)
+VALUES ("Lead Engineer", 90.000, 4);
+INSERT INTO roles (title, salary, departmentID)
+VALUES ("Software Developer", 70.000, 4);
+INSERT INTO roles (title, salary, departmentID)
+VALUES ("Account Manager", 80.000, 3 );
+INSERT INTO roles (title, salary, departmentID)
+VALUES ("Accountant", 60.000, 3);
+INSERT INTO roles (title, salary, departmentID)
+VALUES ("Legal Team Lead", 99.000, 1);
+INSERT INTO roles (title, salary, departmentID)
+VALUES ("Lawyer", 80.001, 1)
+
+INSERT INTO people (firstName, lastName, roleID, managerID)
+VALUES ("Frank", "Cook", 1, 1);
+INSERT INTO people (firstName, lastName, roleID, managerID)
+VALUES ("Monique", "Williams", 3, 2);
+INSERT INTO people (firstName, lastName, roleID, managerID)
+VALUES ("Michael", "Thomas", 2, 1);
+INSERT INTO people (firstName, lastName, roleID, managerID)
+VALUES ("Sam", "Mason", 4, 2);
